c4983eee5dd500bf310215ee1966d8e8484fe022
Vakhid Betrakhmadov <vakhid.dtt@gmail.com>
2019-04-18 00:25:38 +0300

    Added tc folder

---
+++ tc/args_parser.py
@@ -0,0 +1,92 @@
+import argparse
+
+def get_argument_parser():
+    parser = argparse.ArgumentParser(
+        description='Mapping options config.'
+    )
+
+    parser.add_argument(
+        '--autotuner', type=bool, default=True,
+        help='Use autotuner to find best mapping options',
+    )
+
+    parser.add_argument(
+        '--mapToBlocks', type=list,
+        help='The configuration of CUDA grid, i.e. the number of CUDA blocks along three dimensions. Must be within the range allowed by CUDA (maximum 2^31-1 for the first value and 65535 for the second and third). Note that TC mapper eliminates empty blocks and the actual launch size may be smaller than requested.',
+    )
+    parser.add_argument(
+        '--mapToThreads', type=list,
+        help='The configuration of CUDA block, i.e. the number of CUDA threads in each block along three dimensions. Must be within the range allowed by CUDA (maximum 1024 for the first and second value, 32 for the third, product below 1024). Note that TC mapper eliminates empty threads and the actual launch size may be smaller than requested.',
+    )
+    parser.add_argument(
+        '--tile', type=list,
+        help='Perform loop tiling on the generated code with the given sizes. Independent of mapping to a grid of thread blocks.',
+    )
+    parser.add_argument(
+        '--useSharedMemory', type=bool,
+        help='Create block-local copies of data in shared memory when this can leverage data reuse or global memory access coalescing.',
+    )
+    parser.add_argument(
+        '--maxSharedMemory', type=int,
+        help='The amount of shared memory to use, in bytes. If not provided, TC will query the active GPU and use all available shared memory.',
+    )
+    parser.add_argument(
+        '--unroll', type=int,
+        help='Perform loop unrolling on the generated code and produce at most the given number of statements.',
+    )
+    parser.add_argument(
+        '--unrollCopyShared', type=bool,
+        help='Also unroll the copies to and from shared memory introduced by the TC mapper. If unroll value is not provided, has no effect.',
+    )
+    parser.add_argument(
+        '--useReaOnlyCache', type=bool,
+        help='Emit loads to the readonly cache when appropriate.',
+    )
+    parser.add_argument(
+        '--matchLibraryCalls', type=bool,
+        help='Replace computation patterns with calls to highly optimized libraries (such as CUB, CUTLASS) when possible.',
+    )
+    parser.add_argument(
+        '--fixParametersBeforeScheduling', type=bool,
+        help='Perform automatic loop scheduling taking into account specific tensor sizes. May produce faster kernels but significantly increases compilation time. Note that the mapping will be performed for specific tensor sizes anyway.',
+    )
+    parser.add_argument(
+        '--outerScheduleFusionStrategy', type=str,
+        help='Require TC to try and execute different TC expressions interleaved (Max), separately (Min) or interleaved as long as sufficient parallelism is exploited (Preserve3Coincident) by performing loop fusion and fission. Applies before tiling.',
+    )
+    parser.add_argument(
+        '--intraTileFusionStrategy', type=str,
+        help='Require TC to try and execute different TC expressions interleaved (Max), separately (Min) or interleaved as long as sufficient parallelism is exploited (Preserve3Coincident) by performing loop fusion and fission. Applies to inner loops created by tiling.',
+    )
+
+    parser.add_argument(
+        '--reinforce', type=bool, default=False,
+        help='Reinforce the tuning process over time without paying a longer startup cost.',
+    )
+    parser.add_argument(
+        '--tuner_threads', type=int, default=16,
+        help='Number of CPU tuning threads.',
+    )
+    parser.add_argument(
+        '--tuner_generations', type=int, default=25,
+        help='Number of tuning generations.',
+    )
+    parser.add_argument(
+        '--tuner_pop_size', type=int, default=100,
+        help='Number candidates per tuning generations.',
+    )
+    parser.add_argument(
+        '--tuner_number_elites', type=int, default=5,
+        help='Number of best tuning candidates that survive each generation.',
+    )
+    parser.add_argument(
+        '--tuner_devices', type=str, default='0',
+        help='Comma separated list of tuning devices.',
+    )
+    parser.add_argument(
+        '--tuner_cache_file',
+        type=str,
+        default='/tmp/cache_condensenet',
+        help='File to store tuned mapping options',
+    )
+    return parser
---
+++ tc/generate_options.py
@@ -0,0 +1,61 @@
+import argparse
+import torch
+import tensor_comprehensions as tc 
+
+def generate_options(args: argparse.Namespace):
+
+    tuner_config = (
+    tc.TunerConfig()
+    .threads(args.tuner_threads)
+    .generations(args.tuner_generations)
+    .pop_size(args.tuner_pop_size)
+    .number_elites(args.tuner_number_elites)
+    .devices(args.tuner_devices))
+
+    def _generate_options(tc_str: str,
+                        entry_point: str,
+                        *inputs: torch.Tensor) -> tc.MappingOptions:
+        global reinforce
+
+        if entry_point == 'make_idx':
+            return tc.make_naive_options_factory()(tc_str, entry_point, *inputs)
+
+        if args.autotuner:
+            loaded = tc.make_load_from_cache_options_factory(args.tuner_cache_file)(tc_str, entry_point, *inputs) if args.reinforce else None
+            start = loaded if loaded is not None else 'naive'
+
+            return tc.make_autotuned_options_factory(
+                starting_options=start,
+                tuner_config=tuner_config,
+                cache_filename=args.tuner_cache_file,
+                store_to_cache=True,)(tc_str, entry_point, *inputs)
+        else: 
+            options = tc.MappingOptions("naive")
+            if args.mapToBlocks is not None:
+                options.mapToBlocks(args.mapToBlocks)
+            if args.mapToThreads is not None:
+                options.mapToBlocks(args.mapToThreads)
+            if args.tile is not None:
+                options.mapToBlocks(args.tile)
+            if args.useSharedMemory is not None:
+                options.mapToBlocks(args.useSharedMemory)
+            if args.maxSharedMemory is not None:
+                options.mapToBlocks(args.maxSharedMemory)
+            if args.unroll is not None:
+                options.mapToBlocks(args.unroll)
+            if args.unrollCopyShared is not None:
+                options.mapToBlocks(args.unrollCopyShared)
+            if args.useReaOnlyCache is not None:
+                options.mapToBlocks(args.useReaOnlyCache)
+            if args.matchLibraryCalls is not None:
+                options.mapToBlocks(args.matchLibraryCalls)
+            if args.fixParametersBeforeScheduling is not None:
+                options.mapToBlocks(args.fixParametersBeforeScheduling)
+            if args.outerScheduleFusionStrategy is not None:
+                options.mapToBlocks(args.outerScheduleFusionStrategy)
+            if args.intraTileFusionStrategy is not None:
+                options.mapToBlocks(args.intraTileFusionStrategy)
+            
+            return options
+            
+    return _generate_options
