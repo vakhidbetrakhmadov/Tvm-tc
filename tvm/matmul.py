01d3bd949b3c1c2868a6481724e5aa3447c44cb3
Vakhid Betrakhmadov <vakhid.dtt@gmail.com>
2019-04-27 20:28:30 +0300

    Refactoring

--- tvm/matmul.py
+++ tvm/matmul.py
@@ -12,18 +12,18 @@ def matmul_parametric(N, L, M, dtype, args):
     s = tvm.create_schedule(C.op)
 
     # schedule
-    y, x = s[C].op.axis
+    x, y = s[C].op.axis
     k = s[C].op.reduce_axis[0]
 
-    yo, yi = s[C].split(y, args.y)
     xo, xi = s[C].split(x, args.x)
+    yo, yi = s[C].split(y, args.y)
     
-    s[C].reorder(yo, xo, k, yi, xi)
+    s[C].reorder(xo, yo, k, xi, yi)
 
-    s[C].bind(xo, tvm.thread_axis("blockIdx.y"))
-    s[C].bind(xi, tvm.thread_axis("threadIdx.y"))
-    s[C].bind(yo, tvm.thread_axis("blockIdx.x"))
-    s[C].bind(yi, tvm.thread_axis("threadIdx.x"))
+    s[C].bind(xo, tvm.thread_axis("blockIdx.x"))
+    s[C].bind(yo, tvm.thread_axis("threadIdx.x"))
+    s[C].bind(xi, tvm.thread_axis("blockIdx.y"))
+    s[C].bind(yi, tvm.thread_axis("threadIdx.y"))
 
     return s, [A, B, C]
 
