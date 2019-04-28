540ccdd586621aadb14a0c03eed82142698bbe6b
Vakhid Betrakhmadov <vakhid.dtt@gmail.com>
2019-04-28 21:36:05 +0300

    Refactoring

--- tc/programs.py
+++ tc/programs.py
@@ -6,13 +6,12 @@ def matmul(float(M, K) A, float(K, N) B) -> (C) {
 
 MAP = """
 def map(float(M) A) -> (B) {
-    B(m) = A(m) * 3.14
+    B(m) =! A(m) * 3.14
 }
 """
 
 REDUCE = """
 def reduce(float(M) A) -> (B) { 
-    B(n) = 0 where n 0..0
-    B(n) += A(m) where n 0..0
+    B(n) +=! A(m) where n 0..0
 }
 """
