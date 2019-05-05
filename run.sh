a709cf435b7b6160e8af0dc2522485bf20353db4
Vakhid Betrakhmadov <vakhid.dtt@gmail.com>
2019-05-05 13:53:01 +0300

    Refactoring

--- run.sh
+++ run.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 # TC config 
-alias tc_python = '/opt/conda/anaconda/envs/tc_build/bin/python3'
-logfile = "log.txt"
-pop_size = 1
-generations = 1
+run_tc(pop_size, generations, program) {
+    /opt/conda/anaconda/envs/tc_build/bin/python3 --debug=True --autotuner=True --store_to_cache=True --tuner_pop_size=pop_size --tuner_generations=generations --prog=program >> "log.txt"
+}
 
-$tc_python tc/tc_bench.py --debug=True --autotuner=True --store_to_cache=True --tuner_pop_size=$pop_size --tuner_generations=$generations --prog=matmul >> $logfile
-$tc_python tc/tc_bench.py --debug=True --autotuner=True --store_to_cache=True --tuner_pop_size=$pop_size --tuner_generations=$generations --prog=map >> $logfile
-$tc_python tc/tc_bench.py --debug=True --autotuner=True --store_to_cache=True --tuner_pop_size=$pop_size --tuner_generations=$generations --prog=conv2d >> $logfile
-$tc_python tc/tc_bench.py --debug=True --autotuner=True --store_to_cache=True --tuner_pop_size=$pop_size --tuner_generations=$generations --prog=tmm >> $logfile
-$tc_python tc/tc_bench.py --debug=True --autotuner=True --store_to_cache=True --tuner_pop_size=$pop_size --tuner_generations=$generations --prog=tbmm >> $logfile
+for program in matmul, map, conv2d, tmm, tbmm
+do 
+    run_tc(1, 1, program)
+done
