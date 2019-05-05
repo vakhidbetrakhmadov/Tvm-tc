994e89844b568df9410f1f4b00d84824ee03a55c
Vakhid Betrakhmadov <vakhid.dtt@gmail.com>
2019-05-05 18:55:15 +0300

    Refactoring

--- run.sh
+++ run.sh
@@ -9,11 +9,13 @@ run_tc() {
     echo $1 $2 $3 $4
 
     if [ "$3" = "conv2d" ]; then
-        for s in '"50 50 50"'; do
+        for next in '"50 50 50"'; do
+            s = $next
             /opt/conda/anaconda/envs/tc_build/bin/python3 tc/tc_bench.py --debug=True --autotuner=True --store_to_cache=True --tuner_pop_size=$1 --tuner_generations=$2 --prog=$3 --size $s &>> $4
         done
     else 
-        for s in '"256 256 512 14 3"'; do
+        for next in '"256 256 512 14 3"'; do
+            s = $next
             /opt/conda/anaconda/envs/tc_build/bin/python3 tc/tc_bench.py --debug=True --autotuner=True --store_to_cache=True --tuner_pop_size=$1 --tuner_generations=$2 --prog=$3 --size $s &>> $4
         done
     fi 
