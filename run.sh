3c04e10b6b7f994efdb2e355dfc8f0c4824d107d
Vakhid Betrakhmadov <vakhid.dtt@gmail.com>
2019-05-05 14:12:23 +0300

    Refactoring

--- run.sh
+++ run.sh
@@ -2,13 +2,11 @@
 
 # TC config 
 run_tc() {
-    /opt/conda/anaconda/envs/tc_build/bin/python3 --debug=True --autotuner=True --store_to_cache=True --tuner_pop_size=1 --tuner_generations=1 --prog=matmul >> "log.txt"
+    /opt/conda/anaconda/envs/tc_build/bin/python3 tc/tc_bench.py--debug=True --autotuner=True --store_to_cache=True --tuner_pop_size=1 --tuner_generations=1 --prog=matmul >> "log.txt"
     return
 }
 
-run_tc 1 1 matmul "log.txt"
-
-# for program in matmul, map, conv2d, tmm, tbmm
-# do 
-#     run_tc 1 1 program "log.txt"
-# done
+for program in matmul, map, conv2d, tmm, tbmm
+do 
+    run_tc 1 1 program "log.txt"
+done
