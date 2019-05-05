820aab65f2a0e7afa8aa8177e9bb450ae93bfcb1
Vakhid Betrakhmadov <vakhid.dtt@gmail.com>
2019-05-05 17:43:04 +0300

    Refactoring

--- run.sh
+++ run.sh
@@ -8,8 +8,7 @@ logfile="log.txt"
 run_tc() {
     echo $1 $2 $3 $4
 
-    name = "$3"
-    if [$name = "conv2d"]; then
+    if [ "$3" = "conv2d" ]; then
         for s in "'50 50 50'"; do
             /opt/conda/anaconda/envs/tc_build/bin/python3 tc/tc_bench.py --debug=True --autotuner=True --store_to_cache=True --tuner_pop_size=$1 --tuner_generations=$2 --prog=$3 --size $s &>> $4
         done
