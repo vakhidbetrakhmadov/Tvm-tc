c2d32a40008a0a327555fda4798390a915dadbfd
Vakhid Betrakhmadov <vakhid.dtt@gmail.com>
2019-05-05 17:29:38 +0300

    Refactoring

--- run.sh
+++ run.sh
@@ -7,22 +7,25 @@ logfile="log.txt"
 
 run_tc() {
     echo $1 $2 $3 $4
-    /opt/conda/anaconda/envs/tc_build/bin/python3 tc/tc_bench.py --debug=True --autotuner=True --store_to_cache=True --tuner_pop_size=$1 --tuner_generations=$2 --prog=$3 --size $4 &>> $5
-    echo -e "\n\n\n\n" >> $4
+
+    if [$3 = "conv2d"]; then
+        for s in "'50 50 50'"; do
+            /opt/conda/anaconda/envs/tc_build/bin/python3 tc/tc_bench.py --debug=True --autotuner=True --store_to_cache=True --tuner_pop_size=$1 --tuner_generations=$2 --prog=$3 --size $s &>> $4
+        done
+    else 
+        for s in "'256 256 512 14 3'"; do
+            /opt/conda/anaconda/envs/tc_build/bin/python3 tc/tc_bench.py --debug=True --autotuner=True --store_to_cache=True --tuner_pop_size=$1 --tuner_generations=$2 --prog=$3 --size $s &>> $4
+        done
+    fi 
+
+    echo -e "\n - - - - \n" >> $4
     return
 }
 
 rm $logfile
 
-sizes1=('50 50 50')
-for program in matmul map tmm tbmm; do
-    for size in ${sizes1[*]}; do
-        run_tc $pop_size $generations $program $size $logfile
-    done
-done
-
-for size in "'256 256 512 14 3'"; do
-    run_tc $pop_size $generations conv2d $size $logfile
+for program in matmul map conv2d tmm tbmm; do
+    run_tc $pop_size $generations $program $logfile    
 done
 
 #TVM config 
